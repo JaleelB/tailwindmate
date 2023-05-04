5cbc40454f278954130c1c75e730b272913be8c1
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-05-03 23:55:02 -0400

    feat: added tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import { type Config } from "tailwindcss";
+
+export default {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
