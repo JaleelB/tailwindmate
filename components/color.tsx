14c1a0ad6e7dd985cafae57b9576a79b3b9b6dd4
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-05-05 16:34:34 -0400

    update: implement click to copy logic for color component

--- components/color.tsx
+++ components/color.tsx
@@ -1,20 +1,35 @@
-"use client";
 import React, { useState, useRef } from 'react';
 
 interface ColorComponentProps {
-  type: string
-  placeholder: string
+  type: string;
+  placeholder: string;
 }
 
 function ColorComponent({ type, placeholder }: ColorComponentProps) {
-
   const [inputColor, setInputColor] = useState('');
-  const displayedColorRef = useRef('#009abc');
+  const [copyMessage, setCopyMessage] = useState('');
+  const displayedColorRef = useRef('#43e5a2');
 
   const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputColor(event.target.value);
     if (event.target.value) {
       displayedColorRef.current = event.target.value;
+    }
+  };
+
+  const handleColorCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(displayedColorRef.current);
+      setCopyMessage(`Color copied successfully!`);
+      setTimeout(() => {
+        setCopyMessage('');
+      }, 4000);
+    } catch (error) {
+      console.error('Failed to copy color:', error);
+      setCopyMessage(`Failed to copy color code!`);
+      setTimeout(() => {
+        setCopyMessage('');
+      }, 4000);
     }
   };
 
@@ -29,11 +44,17 @@ function ColorComponent({ type, placeholder }: ColorComponentProps) {
           className="p-2 w-full sm:w-72 text-white bg-neutral-900 rounded-md"
         />
       </div>
-      <div className="flex justify-center w-full">
+      <div className="flex justify-center w-full" onClick={() => void handleColorCopy()}>
         <div
           style={{ backgroundColor: displayedColorRef.current }}
-          className="h-32 m-2 w-full rounded-md"
-        ></div>
+          className="h-32 m-2 w-full rounded-md relative cursor-pointer"
+        >
+          <div className="absolute inset-0 flex items-center justify-center text-black">
+            <span>
+              {inputColor || type}: {displayedColorRef.current}
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
