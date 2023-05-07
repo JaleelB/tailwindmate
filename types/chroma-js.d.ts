d89fd0ad97deda314373905087bb6263f66fca81
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-05-07 13:17:50 -0400

    update: hex and color declarations

--- types/chroma-js.d.ts
+++ types/chroma-js.d.ts
@@ -1,9 +1,16 @@
 declare module 'chroma-js' {
-    export interface Chroma {
-      valid: (color: string) => boolean;
-    }
-  
-    const chroma: Chroma;
-    export default chroma;
+  export interface Chroma {
+    (color: string): ChromaInstance;
+    distance(color: chroma.Color, arg1: never): never;
+    valid: (color: string) => boolean;
+  }
+
+  export interface ChromaInstance {
+    hex(): string;
+  }
+
+  const chroma: Chroma;
+  export default chroma;
 }
+
   
