72631e8fad1afb86752e17aea3bc084db0551c19
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-05-14 22:37:29 -0400

    feat: dynamically set text in color component based on contrast of bg color

--- components/color.tsx
+++ components/color.tsx
@@ -60,6 +60,13 @@ function ColorComponent({ type, placeholder }: ColorComponentProps) {
       return { name: 'Invalid', hex: '', distance: -1 };
     }
   }
+
+  const getTextColor = (bgColor: string) => {
+    const whiteContrast = chroma.contrast(bgColor, '#E5E5E5') as number;
+    const blackContrast = chroma.contrast(bgColor, '#292524') as number;
+
+    return whiteContrast > blackContrast ? '#E5E5E5' : '#292524';
+  };
   
 
   return (
@@ -78,7 +85,10 @@ function ColorComponent({ type, placeholder }: ColorComponentProps) {
           style={{ backgroundColor: findColorInTailwind(displayedColorRef.current, lastValidColor.current) }}
           className="h-32 m-2 w-full rounded-md relative cursor-pointer"
         >
-          <div className="absolute inset-0 flex items-center justify-center text-black">
+          <div 
+            className="absolute inset-0 flex items-center justify-center text-black"
+            style={{ color: getTextColor(findColorInTailwind(displayedColorRef.current, lastValidColor.current)) }}
+          >
             <div className='font-medium flex flex-col gap-1 text-center'>
               <span className='text-2xl'>{colorName.current}</span>
               <span className='text-sm p-1'>{displayedColorRef.current}</span>  
