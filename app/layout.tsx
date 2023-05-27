411dbf775796d0246d1886f8e9ccce35caa53ce1
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-05-26 23:31:20 -0400

    feat: added metadata config and website product shot

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,32 @@
 import '../styles/globals.css'
 
+export const metadata = {
+    title: "Tailwindmate",
+    description: "A collection of all chatgpt plugins",
+    twitter: {
+      card: "summary_large_image",
+      title: "Tailwindmate",
+      description: "Easily convert between Tailwind CSS classes and color formats",
+      creator: "@jal_eelll",
+      images: ["https://tailwindmate.jaleelbennett.com/web-shot.png"],
+    },
+    openGraph: {
+      title: "Tailwindmate",
+      description: "A versatile color conversion web app for Tailwind CSS",
+      type: "website",
+      url: "https://tailwindmate.jaleelbennett.com/",
+      images: [
+        {
+          url: "https://tailwindmate.jaleelbennett.com/web-shot.png",
+          width: 1200,
+          height: 715,
+          alt: "Tailwindmate",
+        },
+      ],
+    },
+};
+  
+
 export default function RootLayout({
     children,
 }: {
